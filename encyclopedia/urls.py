a1daca17ca502ca463b0662c1160a3f582e39bcc
SBMOYO <sunganomoyo1@gmail.com>
2023-05-17 15:32:34 +0200

    created a search view

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:title>", views.get_title, name="get_title")
+    path("<str:title>", views.get_title, name="get_title"),
+    path("search", views.search, name="search_title")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render, HttpResponse
+from django import forms
 import markdown
 
 from . import util
 
 
+class Search(forms.Form):
+    q = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Search encyclopedia...'}))
+
+
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "form": Search()
     })
+
 
 def get_title(request, title):
     content = util.get_entry(title)
@@ -20,4 +27,12 @@ def get_title(request, title):
         "title": title,
         "content": content
     })
+
+
+def search(request):
+
+    if request.method == 'POST':
+        form = search(request.POST)
+        if form.is_valid():
+            q = form.cleaned_data['q']
 
